3a768a3507624b96495a004996ed5279c51af3a5
bear-KU <pl4k3aju@s.okayama-u.ac.jp>
2026-03-05 19:28:34 +0900

    Reduce high-speed mode from 240 fps to 120 fps due to MediaCodec decoding overhead
    - 240 fps caused frame rate drop to ~200 fps
    - Currently set to 120 fps to ensure stable performance

--- app/src/main/java/com/lumitalk/util/camera/CameraConfig.kt
+++ app/src/main/java/com/lumitalk/util/camera/CameraConfig.kt
@@ -15,4 +15,5 @@ data class CameraConfig(
 }
 
 val STANDARD_CONFIG = CameraConfig(640, 480, 30, CameraMode.STANDARD)
-val HIGH_SPEED_CONFIG = CameraConfig(1920, 1080, 240, CameraMode.HIGH_SPEED)
+val HIGH_SPEED_CONFIG = CameraConfig(1920, 1080, 120, CameraMode.HIGH_SPEED)
+// val HIGH_SPEED_CONFIG = CameraConfig(1920, 1080, 240, CameraMode.HIGH_SPEED)
